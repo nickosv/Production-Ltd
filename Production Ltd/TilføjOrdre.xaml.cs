c6bcab1209fe7a8fb56ebe32416341c50f48afe1
Schandorff <schandorff1@hotmail.com>
2014-12-04 11:21:39 +0100

    HER

--- Production Ltd/TilføjOrdre.xaml.cs
+++ Production Ltd/TilføjOrdre.xaml.cs
@@ -10,29 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Production_Ltd
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for TilføjOrdre.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class TilføjOrdre : Window
     {
-        public MainWindow()
+        public TilføjOrdre()
         {
             InitializeComponent();
-        }
-
-        private void tilføjKunde(object sender, RoutedEventArgs e)
-        {
-            new TilføjKunde().Show();
-        }
-
-        private void tilføjOrdre(object sender, RoutedEventArgs e)
-        {
-            new TilføjOrdre().Show();
         }
     }
 }
