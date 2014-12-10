16413fffb4cb64e77b96d2c4013a0f81427f9a3d
fred3760 <fred3760@edu.eal.dk>
2014-12-10 09:10:44 +0100

    Listview interface methods

---
+++ Production Ltd/Ordre.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Production_Ltd
+{
+    class Ordre
+    {
+        public Ordre(string InOrdreID, string inAntal, string inProdukttype, string inDeadline, string inKunde)
+        {
+            InOrdreID = OrdreID;
+            inAntal = Antal;
+            inProdukttype = Produkttype;
+            inDeadline = Deadline;
+            inKunde = Kunde;
+        }
+
+        public string OrdreID { get; set; }
+        public string Deadline { get; set; }
+        public string Antal { get; set; }
+        public string Kunde { get; set; }
+        public string Produkttype { get; set; }
+
+    }
+}
